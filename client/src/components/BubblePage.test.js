e88deeb8a3730aa90498cdcd582491c923113e0b
Michael Sanchez <michaelsanchez42@live.com>
2020-10-12 21:21:13 -0600

    mvp

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -3,50 +3,106 @@ import { render, screen, waitFor , act, queryAllByTestId } from "@testing-librar
 import BubblePage from "./BubblePage";
 
 
-// const bogusBubbleData = [
-//     {
-//       color: "aqua",
-//       code: {
-//         hex: "#00ffff"
-//       },
-//       id: 3
-//     },
-//     {
-//       color: "aquamarine",
-//       code: {
-//         hex: "#7fffd4"
-//       },
-//       id: 4
-//     },
-//     {
-//       color: "lilac",
-//       code: {
-//         hex: "#9a99dd"
-//       },
-//       id: 5
-//     },
-//     {
-//       color: "softpink",
-//       code: {
-//         hex: "#dd99ba"
-//       },
-//       id: 6
-//     },
-//     {
-//       color: "bisque",
-//       code: {
-//         hex: "#dd9a99"
-//       },
-//       id: 7
-//     },
-//     {
-//       color: "softyellow",
-//       code: {
-//         hex: "#dcdd99"
-//       },
-//       id: 8
-//   }
-// ]
+const bogusBubbleData = [
+    {
+      color: "aqua",
+      code: {
+        hex: "#00ffff"
+      },
+      id: 3
+    },
+    {
+      color: "aquamarine",
+      code: {
+        hex: "#7fffd4"
+      },
+      id: 4
+    },
+    {
+      color: "lilac",
+      code: {
+        hex: "#9a99dd"
+      },
+      id: 5
+    },
+    {
+      color: "softpink",
+      code: {
+        hex: "#dd99ba"
+      },
+      id: 6
+    },
+    {
+      color: "bisque",
+      code: {
+        hex: "#dd9a99"
+      },
+      id: 7
+    },
+    {
+      color: "softyellow",
+      code: {
+          hex: "#dcdd99"
+        },
+        id: 8
+    }
+  ]
+jest.mock(bogusBubbleData)
+  test("Fetches data and renders the bubbles", async () => {
+    bogusBubbleData.mockResolvedValueOnce({
+      message: [  
+        {
+        color: "aqua",
+        code: {
+          hex: "#00ffff"
+        },
+        id: 3
+      },
+      {
+        color: "aquamarine",
+        code: {
+          hex: "#7fffd4"
+        },
+        id: 4
+      },
+      {
+        color: "lilac",
+        code: {
+          hex: "#9a99dd"
+        },
+        id: 5
+      },
+      {
+        color: "softpink",
+        code: {
+          hex: "#dd99ba"
+        },
+        id: 6
+      },
+      {
+        color: "bisque",
+        code: {
+          hex: "#dd9a99"
+        },
+        id: 7
+      },
+      {
+        color: "softyellow",
+        code: {
+            hex: "#dcdd99"
+          },
+          id: 8
+      }
+    ]
+    });
+
+    const 
+
+
+
+
+
+
 
 // test("Fetches data and renders the bubbles", () => {
 //   // Finish this test
@@ -79,31 +135,3 @@ import BubblePage from "./BubblePage";
 //         id: 5
 //       }
 //     ]
-// test("Fetches data and renders the bubbles", () => {
-//   const {container, queryAllByTestId, rerender, act, screen} = render(<BubblePage />)
-//   act(() => container = render(<BubblePage />))
-
-//   await waitfor (() => {
-//     const colors = container
-
-// })
-
-// act(() => container = render(<BubblePage ))
-// await waitfor (() => {
-//   const colorItems = contain
-// })
-jest.mock("./components/BubblePage");
-const mockData = [];
-
-test("renders bubblepage", () => {
-  render(<BubblePage />)
-})
-
-test("renders ColorList and BUbbles", () => {
-  render(<ColorList colors={[]}/>)
-})
-
-test("fetches data nad renders the bubbles", async () => {
-mock
-
-})
